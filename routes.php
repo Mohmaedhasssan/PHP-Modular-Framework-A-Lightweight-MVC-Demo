5132ca4137ff0c9a15cb6c2edeced5fa353f5617
mohmead hasssan <mohamed.hassan.official55@gmail.com>
2024-12-21 00:49:30 +0200

    created a note folder

--- routes.php
+++ routes.php
@@ -3,9 +3,9 @@
 return [
     '/Demo/' => 'controllers/index.php',
     '/Demo/about' => 'controllers/about.php',
-    '/Demo/notes' => 'controllers/notes.php',
-    '/Demo/note' => 'controllers/note.php',
-    '/Demo/note/create' => 'controllers/create-note.php',
+    '/Demo/notes' => 'controllers/notes/index.php',
+    '/Demo/note' => 'controllers/notes/show.php',
+    '/Demo/note/create' => 'controllers/notes/create.php',
     '/Demo/contact' => 'controllers/contact.php',
     
 ];
