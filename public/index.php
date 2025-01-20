750cfbdce49294ac2f45255fc32e98eeb125dd34
mohmead hasssan <mohamed.hassan.official55@gmail.com>
2025-01-20 11:29:30 +0200

    Add Composer autoloader

--- public/index.php
+++ public/index.php
@@ -4,16 +4,14 @@ use Core\Router;
 use Core\Session;
 use Core\Validation\ValidationException;
 
-session_start();
-
 const BASE_PATH = __DIR__ . "/../";
 
-require BASE_PATH . "Core/functions.php";
+require __DIR__ . "/../vendor/autoload.php";
 
-spl_autoload_register(function ($class) {
-    // $class=str_replace("\\","/",$class);    //the path works with this / and \ as separator
-    require base_path("{$class}.php");
-});
+session_start();
+
+
+require BASE_PATH . "Core/functions.php";
 
 
 require base_path('bootstrap.php');
@@ -25,10 +23,10 @@ $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER["REQUEST_URI"])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
-try{
-    $router->route($uri,$method);
-}catch(ValidationException $e){
-    
+try {
+    $router->route($uri, $method);
+} catch (ValidationException $e) {
+
     Session::flash('errors', $e->errors);
     Session::flash('old', $e->old);
 
